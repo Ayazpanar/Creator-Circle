b15899cc99e06bba1e7faa4fb144ca02910587b9
Ayaz Panar <ayazpanar0786@gmail.com>
2025-11-15 23:54:50 +0530

    feat: implement user authentication with Clerk and add layout components

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,16 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Creator Circle",
-  description: "A community for creators to connect, collaborate, and grow together.",
+  description:
+    "A community for creators to connect, collaborate, and grow together.",
 };
 
 export default function RootLayout({ children }) {
@@ -20,12 +24,19 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          <ConvexClientProvider>
-          {/* Header */}
-          <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
-            {children}
-          </main>
-          </ConvexClientProvider>
+          <ClerkProvider
+            appearance={{
+              theme: shadesOfPurple,
+            }}
+          >
+            <ConvexClientProvider>
+              {/* Header */}
+              <Header />
+              <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+                {children}
+              </main>
+            </ConvexClientProvider>
+          </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
