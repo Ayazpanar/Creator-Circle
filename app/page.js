b0b9aef0506b020bdaf1515856a0418d53e3b8dd
Ayaz Panar <ayazpanar0786@gmail.com>
2025-11-20 23:31:10 +0530

    feat: add Features component and custom styles for landing page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Features from "@/components/features";
 import HeroSection from "@/components/hero-section";
 import MouseEffect from "@/components/mouse-effect";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <div className="fixed inset-0 bg-linear-to-br from-purple-900/20 via-blue-900/20 to-green-900/20 animate-pulse" />
       <MouseEffect />
       <HeroSection />
+      <Features />
     </div>
   );
 }
