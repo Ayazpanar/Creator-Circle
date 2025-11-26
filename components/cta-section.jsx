b5eff43f274b3e8d0b1d247561f6c273ccf96ba7
Ayaz Panar <ayazpanar0786@gmail.com>
2025-11-26 16:06:53 +0530

    feat: add ArrowRight icon to the primary button in CtaSection component

--- components/cta-section.jsx
+++ components/cta-section.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 const CtaSection = () => {
   return (
@@ -22,6 +23,7 @@ const CtaSection = () => {
             size="xl"
           >
             Start Creating for Free
+            <ArrowRight className="h-5 w-5" />
           </Button>
         </Link>
         <Link href="/feed">
