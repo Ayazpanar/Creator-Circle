73c999d8b211463a4983570ac80e38d314f4054b
Ayaz Panar <ayazpanar0786@gmail.com>
2025-11-26 21:12:00 +0530

    feat: add Footer component with social links and integrate it into RootLayout

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,5 +1,13 @@
 import { Heart } from "lucide-react";
 import React from "react";
+import {
+  TbBrandGithub,
+  TbBrandLinkedin,
+  TbBrandWhatsapp,
+} from "react-icons/tb";
+import { LuExternalLink } from "react-icons/lu";
+import { SiGmail } from "react-icons/si";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -8,11 +16,44 @@ const Footer = () => {
         <p className="text-muted-foreground">
           Made with
           <Heart className="inline-block w-5 h-5 m-1 text-pink-600 animate-pulse fill-pink-600" />
-          by{" "}
-          <span className="font-bold bg-linear-to-r from-purple-700 to-blue-700 bg-clip-text text-transparent">
+          by
+          <span className="font-bold bg-linear-to-r ml-1 from-purple-700 to-blue-700 bg-clip-text text-transparent">
             Ayazpanar.
-          </span>{" "}
+          </span>
         </p>
+        <div className="mt-2 flex gap-2 sm:gap-3 items-center justify-center">
+          <Link
+            href="https://linkedin.com/in/ayazpanar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TbBrandLinkedin className="inline-block h-6 w-6 hover:text-white cursor-pointer" />
+          </Link>
+          <Link
+            href="https://github.com/ayazpanar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TbBrandGithub className="inline-block h-6 w-6 border-2 border-amber-50/40 hover:border-white rounded-full p-1 hover:text-white cursor-pointer" />
+          </Link>
+          <Link
+            href="https://wa.me/+919909130787"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TbBrandWhatsapp className="inline-block h-6 w-6 hover:border-white hover:text-white cursor-pointer" />
+          </Link>
+          <Link href="mailto:ayazpanar0786@gmail.com">
+            <SiGmail className="inline-block h-6 w-6 text-xs text hover:text-white cursor-pointer" />
+          </Link>
+          <Link
+            href="https://ayazpanar-portfolio.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LuExternalLink className="inline-block h-6 w-6 hover:text-white cursor-pointer" />
+          </Link>
+        </div>
       </div>
     </footer>
   );
